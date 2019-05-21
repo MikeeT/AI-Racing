338569fa159bdf2d89d9490a3fba0737afa2ed6e
Michael Topolski <MikeeT@users.noreply.github.com>
2019-05-20 21:16:22 -0400

    Initial Setup
    created initial player class and main file

---
+++ Player.java
@@ -0,0 +1,10 @@
+public class Player{
+
+    boolean isDead;
+    int score;
+    int gen = 0;
+
+    Player() {
+
+    }
+}
---
+++ main.java
@@ -0,0 +1,8 @@
+//main file used to control racecar
+
+public class MyMainClass {
+    public static void main(String [ ] args){
+
+        Player arty;
+    }
+}
